1b4e4a086ab425185ebe3370d19d824a0527717a
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-12-28 15:24:44 +0600

    finidh homework lesson5. CRUD

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,15 +10,17 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      now: new Date(),
-      isBtnClicked: false,
-      name: '',
       posts: [],
+      showAddModal: false,
+      addAuthor: '',
+      addTitle: '',
+      addContent: '',
+      showEditModal: false,
+      editAuthor: '',
+      editTitle: '',
+      editContent: '',
+      editPostId: 0,
     };
-
-    this.handleClick = this.handleClick.bind(this);
-    this.handleForm = this.handleForm.bind(this);
-    this.changeName = this.changeName.bind(this);
   }
 
   componentDidMount() {
@@ -33,41 +35,19 @@ class App extends React.Component {
       })
   }
 
-  // componentDidMount() {
-  //   setInterval(
-  //     () => {
-  //       this.tick()
-  //     }, 1000
-  //   );
-  // }
-  //
-  // tick() {
-  //   this.setState({
-  //     now: new Date()
-  //   })
-  // }
-
-  handleClick() {
-    console.log('Clicked');
-
-    this.setState({
-      isBtnClicked: true,
+  renderPosts = (posts) => {
+    return posts.map((post) => {
+      return <Row
+        key={post._id}
+        title={post.title}
+        content={post.content}
+        author={post.author}
+        _id={post._id}
+        handleDelete={this.handleDelete.bind(this)}
+        handleEdit={this.handleEdit.bind(this)}
+      />
     })
-  }
-
-  handleForm(event) {
-    event.preventDefault();
-    console.log('FORM SENT');
-    const name = this.state.name;
-    console.log('отправлены данные ', name);
-  }
-
-  changeName(event) {
-    console.log(event.target.value);
-    this.setState({
-      name: event.target.value,
-    });
-  }
+  };
 
   handleDelete(deleteId) {
     console.log('handleDelete ', deleteId);
@@ -83,48 +63,213 @@ class App extends React.Component {
       }).catch((error) => {
         console.log(error);
       });
-  }
+  };
+
+  handleEdit = (id, author, title, content) => {
+    this.setState({
+      showEditModal: true,
+      editAuthor: author,
+      editTitle: title,
+      editContent: content,
+      editPostId: id
+    });
+  };
+
+  handleAddShowModal = (event) => {
+    event.preventDefault();
+    const showAddModal = this.state.showAddModal;
+
+    this.setState({
+      showAddModal: !showAddModal,
+      addAuthor: '',
+      addTitle: '',
+      addContent: '',
+    })
+
+  };
+
+  handleEditShowModal = (event) => {
+    event.preventDefault();
+    const showEditModal = this.state.showEditModal;
+
+    this.setState({
+      showEditModal: !showEditModal,
+      editAuthor: '',
+      editTitle: '',
+      editContent: '',
+      editPostId: 0,
+    });
+  };
+
+  handleModalInputsChange = (event) => {
+    event.preventDefault();
+    const inputValue = event.target.value;
+    const inputName = event.target.name;
+
+    this.setState({
+      [inputName]: inputValue,
+    })
+  };
+
+  handleAddPost = (event) => {
+    const addAuthor = this.state.addAuthor;
+    const addTitle = this.state.addTitle;
+    const addContent = this.state.addContent;
+
+    const newPost = {
+      author: addAuthor,
+      title: addTitle,
+      content: addContent
+    };
+
+    axios.post('http://localhost:3001/api/posts', newPost)
+      .then((success) => {
+        console.log('success ', success);
+        if (success.status === 200) {
+          const savedPost = success.data.savedPost;
+          const posts = this.state.posts;
+          posts.push(savedPost);
+          this.setState({
+            posts: posts,
+            showAddModal: false,
+            addAuthor: '',
+            addTitle: '',
+            addContent: '',
+          })
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  handleEditPost = (event) => {
+    event.preventDefault();
+    const editAuthor = this.state.editAuthor;
+    const editTitle = this.state.editTitle;
+    const editContent = this.state.editContent;
+    const editPostId = this.state.editPostId;
+
+    const updatePost = {
+      author: editAuthor,
+      title: editTitle,
+      content: editContent,
+    };
+
+    axios.put('http://localhost:3001/api/posts/' + editPostId, updatePost)
+      .then((success) => {
+        console.log('success ', success);
+        if (success.status === 201) {
+          const savedPost = success.data.updatePost;
+          let posts = this.state.posts;
+          const index = posts.findIndex(post => post._id === savedPost._id);
+          console.log(posts);
+          console.log(index);
+          if (index >= 0) {
+            posts.splice(index, 1, savedPost);
+          }
+
+          this.setState({
+            posts: posts,
+            showEditModal: false,
+            editAuthor: '',
+            editTitle: '',
+            editContent: '',
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   render() {
-    const now = this.state.now.toLocaleTimeString();
-    const isBtnClicked = this.state.isBtnClicked ? <p>Clicked</p> : <p>Not clicked</p>;
-    const isBtnClickedState = this.state.isBtnClicked;
-
-    const name = this.state.name;
-
     const posts = this.state.posts;
+    const showAddModal = this.state.showAddModal;
+    const addAuthor = this.state.addAuthor;
+    const addTitle = this.state.addTitle;
+    const addContent = this.state.addContent;
+
+    const showEditModal = this.state.showEditModal;
+    const editAuthor = this.state.editAuthor;
+    const editTitle = this.state.editTitle;
+    const editContent = this.state.editContent;
+
 
     return (
-      <div>
+      <div className="px-2 py-2">
 
         <Header/>
-        <div>{ now }</div>
-        <ul>
-          {
-            posts.map((post) => {
-              return <Row
-                key={post._id}
-                title={post.title}
-                content={post.content}
-                author={post.author}
-                _id={post._id}
-                handleDelete={this.handleDelete.bind(this)}
-              />
-            })
-          }
+
+        {showAddModal &&
+        <div className="modal">
+          <div className="modal-content">
+            <div className="modal-header py-2">
+              <h2>Добавить новый пост</h2>
+              <span className="close" onClick={this.handleAddShowModal.bind(this)}>&times;</span>
+            </div>
+            <div className="modal-body">
+              <div className="input-group my-3">
+                <input type="text" className="form-control" name="addAuthor" placeholder="Author"
+                       onChange={this.handleModalInputsChange.bind(this)} value={addAuthor}/>
+              </div>
+              <div className="input-group">
+                <input type="text" className="form-control" name="addTitle" placeholder="Title"
+                       onChange={this.handleModalInputsChange.bind(this)} value={addTitle}/>
+              </div>
+              <div className="input-group my-3">
+                <textarea type="text" className="form-control" name="addContent" placeholder="Content..."
+                          onChange={this.handleModalInputsChange.bind(this)} value={addContent}/>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <div className="my-2">
+                <button className="btn btn-secondary mr-2" onClick={this.handleAddShowModal.bind(this)}>Отменить</button>
+                <button className="btn btn-success" onClick={this.handleAddPost.bind(this)}>Добавить</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        }
+
+        <div>
+          <button className="btn btn-primary my-2" onClick={this.handleAddShowModal.bind(this)}><i className="fas fa-plus-circle"></i></button>
+        </div>
+        <ul className="list-group">
+          { this.renderPosts(posts) }
         </ul>
-        App component
-        <button onClick={this.handleClick}>Click me!</button>
-        {/*{isBtnClicked}*/}
 
-        {isBtnClickedState && <p>Clicked!</p>}
-        {!isBtnClickedState && <p>Not clicked</p>}
 
-        <form>
-          <input type="text" value={name} placeholder="Name" onChange={this.changeName}/>
-          <button onClick={this.handleForm}>Send</button>
-        </form>
-
+        {showEditModal &&
+        <div className="modal">
+          <div className="modal-content">
+            <div className="modal-header py-2">
+              <h2>Отредактировать пост</h2>
+              <span className="close" onClick={this.handleEditShowModal.bind(this)}>&times;</span>
+            </div>
+            <div className="modal-body">
+              <div className="input-group my-3">
+                <input type="text" className="form-control" name="editAuthor" placeholder="Author"
+                       onChange={this.handleModalInputsChange.bind(this)} value={editAuthor}/>
+              </div>
+              <div className="input-group">
+                <input type="text" className="form-control" name="editTitle" placeholder="Title"
+                       onChange={this.handleModalInputsChange.bind(this)} value={editTitle}/>
+              </div>
+              <div className="input-group my-3">
+                <textarea type="text" className="form-control" name="editContent" placeholder="Content..."
+                          onChange={this.handleModalInputsChange.bind(this)} value={editContent}/>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <div className="my-2">
+                <button className="btn btn-secondary mr-2" onClick={this.handleEditShowModal.bind(this)}>Отменить</button>
+                <button className="btn btn-success" onClick={this.handleEditPost.bind(this)}>Изменить</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        }
       </div>
     )
   }
--- client/src/components/Row.jsx
+++ client/src/components/Row.jsx
@@ -12,7 +12,12 @@ class Row extends Component {
   }
 
   editPost() {
+    const id = this.props._id;
+    const author = this.props.author;
+    const title = this.props.title;
+    const content = this.props.content;
 
+    this.props.handleEdit(id, author, title, content);
   }
 
   render() {
@@ -21,9 +26,9 @@ class Row extends Component {
     const content = this.props.content;
 
     return (
-      <li>{title}, {content}, {author}
-        <button onClick={this.deletePost.bind(this)}>Delete post</button>
-        <button onClick={this.editPost.bind(this)}>Edit post</button>
+      <li className="list-group-item">{title}, {content}, {author}
+        <button onClick={this.deletePost.bind(this)} className="btn btn-danger ml-2"><i className="fas fa-trash-alt"></i></button>
+        <button onClick={this.editPost.bind(this)} className="btn btn-info ml-2"><i className="fas fa-edit"></i></button>
       </li>
     );
   }
